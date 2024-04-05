c1967725d2a71ddedaa22159ebd1bf7517a46ef6
thaitbph37183 <150651805+thaitbph37183@users.noreply.github.com>
2024-04-05 14:18:10 +0700

    lab7

---
+++ lab7/product.js
@@ -0,0 +1,18 @@
+[
+    { "name": "Aniseed Syrup", "price": 190, "discount": 0.19, "date": "2000-03-16" },
+    { "name": "Change", "price": 19, "discount": 5, "date": "1982-12-18" },
+    { "name": "Product", "price": 19, "discount": 10, "date": "1982-12-18" },
+    { "name": "Honey", "price": 19, "discount": 13, "date": "1982-12-18" },
+    { "name": "Aniseed Syrup", "price": 190, "discount": 0.19, "date": "2000-03-16" },
+    { "name": "Change", "price": 19, "discount": 5, "date": "1982-12-18" },
+    { "name": "Product", "price": 19, "discount": 10, "date": "1982-12-18" },
+    { "name": "Honey", "price": 19, "discount": 13, "date": "1982-12-18" },
+    { "name": "Aniseed Syrup", "price": 190, "discount": 0.19, "date": "2000-03-16" },
+    { "name": "Change", "price": 19, "discount": 5, "date": "1982-12-18" },
+    { "name": "Product", "price": 19, "discount": 10, "date": "1982-12-18" },
+    { "name": "Honey", "price": 19, "discount": 13, "date": "1982-12-18" },
+    { "name": "Aniseed Syrup", "price": 190, "discount": 0.19, "date": "2000-03-16" },
+    { "name": "Change", "price": 19, "discount": 5, "date": "1982-12-18" },
+    { "name": "Product", "price": 19, "discount": 10, "date": "1982-12-18" },
+    { "name": "Honey", "price": 19, "discount": 13, "date": "1982-12-18" }
+]
